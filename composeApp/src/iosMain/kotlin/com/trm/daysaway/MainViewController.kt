6666baa7b20ee4a7f4855e5be40213a0da188885
tosoba <therealmerengue@gmail.com>
2025-11-26 20:45:56 +0100

    Connect FakeWidgetManager on iOS temporarily

--- composeApp/src/iosMain/kotlin/com/trm/daysaway/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/trm/daysaway/MainViewController.kt
@@ -2,5 +2,6 @@ package com.trm.daysaway
 
 import androidx.compose.ui.window.ComposeUIViewController
 import com.trm.daysaway.ui.App
+import com.trm.daysaway.widget.FakeWidgetManager
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController { App(FakeWidgetManager()) }
